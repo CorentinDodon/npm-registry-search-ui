c54ad18acd472bf79a5adcc973b8e5b6fa5334d9
Corentin Dodon <corentin.dodon@gmail.com>
2023-03-20 22:58:58 +0100

    Create PackageDataGrid component + move it away from index.tsx, add pagination & implement api call

---
+++ components/PackageDataGrid.tsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from 'react'
+import Button from '@mui/material/Button'
+import {
+  DataGrid,
+  GridColDef,
+  GridRowParams,
+  gridClasses,
+} from '@mui/x-data-grid'
+import { PackageListInterface } from '@/interfaces/PackageListInterface'
+import { searchNpmRegistry } from '@/api/npm-registry'
+
+function visualizePackage(packageName: string) {
+  console.log(packageName)
+}
+
+type PackageDataGridProps = {
+  search: string
+}
+export default function PackageDataGrid({ search }: PackageDataGridProps) {
+  const [packageList, setPackageList] = useState<PackageListInterface>({
+    objects: [],
+    total: 0,
+  })
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: 20,
+  })
+
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Package Name',
+      flex: 0.2,
+      valueGetter: (params) => {
+        return params.row.package.name
+      },
+    },
+    {
+      field: 'author',
+      headerName: 'Author',
+      flex: 0.2,
+      valueGetter: (params) => {
+        return params.row.package.publisher.username
+      },
+    },
+    {
+      field: 'version',
+      headerName: 'Version',
+      flex: 0.2,
+      valueGetter: (params) => {
+        return params.row.package.version
+      },
+    },
+    {
+      field: 'last_updated',
+      headerName: 'Last updated',
+      flex: 0.2,
+      valueGetter: (params) => {
+        return params.row.package.date
+      },
+    },
+    {
+      field: 'visualize',
+      headerName: 'Visualize package',
+      flex: 0.1,
+      renderCell: ({ row }: Partial<GridRowParams>) => (
+        <Button onClick={() => visualizePackage(row.name)}>Action</Button>
+      ),
+    },
+  ]
+
+  useEffect(() => {
+    if (!search) return
+    searchNpmRegistry(search, paginationModel, setPackageList)
+  }, [search, paginationModel])
+
+  return (
+    <DataGrid
+      rows={packageList.objects}
+      getRowId={(row) => row.package.name}
+      columns={columns}
+      autoHeight
+      rowSelection={false}
+      rowCount={packageList.total}
+      pageSizeOptions={[10, 20, 50, 100]}
+      paginationModel={paginationModel}
+      paginationMode="server"
+      onPaginationModelChange={setPaginationModel}
+      sx={{
+        display: 'flex',
+        width: '90%',
+        margin: 'auto',
+        [`& .${gridClasses.cell}:focus, & .${gridClasses.cell}:focus-within`]: {
+          outline: 'none',
+        },
+        [`& .${gridClasses.columnHeader}:focus, & .${gridClasses.columnHeader}:focus-within`]:
+          {
+            outline: 'none',
+          },
+      }}
+    />
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,18 @@
+import { useState } from 'react'
 import Head from 'next/head'
-import Grid from '@mui/material/Typography'
-import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
 import SearchButton from '@/components/SearchButton'
 import CustomAppBar from '@/components/CustomAppBar'
-import {
-  DataGrid,
-  GridRowsProp,
-  GridColDef,
-  GridRowParams,
-  gridClasses,
-} from '@mui/x-data-grid'
-import type {} from '@mui/x-data-grid/themeAugmentation'
-
-const rows: GridRowsProp = [
-  { id: 1, package_name: 'Hello', author: 'World', last_updated: '12-01-2020' },
-  {
-    id: 2,
-    package_name: 'DataGridPro',
-    author: 'is Awesome',
-    last_updated: '12-01-2020',
-  },
-  {
-    id: 3,
-    package_name: 'MUI',
-    author: 'is Amazing',
-    last_updated: '12-01-2020',
-  },
-]
-
-function visualizePackage(packageName: string) {
-  console.log(packageName)
-}
-
-const columns: GridColDef[] = [
-  { field: 'package_name', headerName: 'Package Name', flex: 0.2 },
-  { field: 'author', headerName: 'Author', flex: 0.2 },
-  { field: 'last_updated', headerName: 'Last updated', flex: 0.2 },
-  {
-    field: 'visualize',
-    headerName: 'Visualize package',
-    flex: 0.1,
-    renderCell: ({ row }: Partial<GridRowParams>) => (
-      <Button onClick={() => visualizePackage(row.name)}>Action</Button>
-    ),
-  },
-]
+import PackageDataGrid from '@/components/PackageDataGrid'
 
 export default function Home() {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = async (e: React.FormEvent, search: string) => {
+    e.preventDefault()
+    setSearch(search)
+  }
+
   return (
     <>
       <Head>
@@ -55,30 +21,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <CustomAppBar />
-      <Grid sx={{ p: 2 }}>
-        <SearchButton />
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          autoHeight
-          loading={false}
-          rowSelection={false}
-          sx={{
-            display: 'flex',
-            width: '90%',
-            margin: 'auto',
-            [`& .${gridClasses.cell}:focus, & .${gridClasses.cell}:focus-within`]:
-              {
-                outline: 'none',
-              },
-            [`& .${gridClasses.columnHeader}:focus, & .${gridClasses.columnHeader}:focus-within`]:
-              {
-                outline: 'none',
-              },
-          }}
-        />
-      </Grid>
+      <div>
+        <CustomAppBar />
+        <Grid sx={{ p: 2 }}>
+          <SearchButton submit={handleSearch} />
+          <PackageDataGrid search={search} />
+        </Grid>
+      </div>
     </>
   )
 }
