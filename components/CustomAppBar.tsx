08ee02ca85ada28f8ba74998b419eb94299784e2
Corentin Dodon <corentin.dodon@gmail.com>
2023-03-21 22:56:45 +0100

    update CustomAppBar

--- components/CustomAppBar.tsx
+++ components/CustomAppBar.tsx
@@ -1,15 +1,30 @@
 import AppBar from '@mui/material/AppBar'
 import Typography from '@mui/material/Typography'
 import Toolbar from '@mui/material/Toolbar'
+import HomeIcon from '@mui/icons-material/Home'
+import Link from '@mui/material/Link'
+import NextLink from 'next/link'
 
 export default function CustomAppBar() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div">
-          News
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Link
+            sx={{ display: 'flex', alignItems: 'center' }}
+            underline="none"
+            color="inherit"
+            href="/"
+            component={NextLink}
+          >
+            <HomeIcon />
+            <Typography variant="h6" component="div">
+              Search NPM Registry
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
   )
 }
